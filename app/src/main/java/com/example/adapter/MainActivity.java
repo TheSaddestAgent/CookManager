82561d930027a03be01ff13a19384f10ab62fa33
TheSaddestAgent <20080505slava@gmail.com>
2024-05-19 17:26:56 +0300

    preparation for downloading files from github, not from the original site2

--- app/src/main/java/com/example/adapter/MainActivity.java
+++ app/src/main/java/com/example/adapter/MainActivity.java
@@ -78,6 +78,10 @@ public class MainActivity extends Activity {
                 "10. Place in the prepared loaf pans, and allow to rise until dough has topped the pans by one inch, another 45 minutes to 1 hour.\n\n" +
                 "11. Meanwhile, preheat the oven to 350 degrees F (175 degrees C). Bake the risen loaves in the preheated oven until golden brown for 25 to 30 minutes, do not overbake.\n\n" +
                 "12. Lightly brush the tops of the loaves with remaining 2 tablespoons melted butter when done to prevent crust from getting hard.\n\n";
+        gson = new Gson();
+        json = gson.toJson(dish);
+        prefsEditor.putString("Wheat Bread", json);
+
 
         dish = new Dish();
         dish.strURL = "https://www.allrecipes.com/thmb/G1L_0_o8euNngYcOuX3BUChWp7w=/0x512/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7968859_Green-Lentil-Gumbo-Soup_Chef-John_4x3-060776d1bb394ba0a08ebc05d1a4e90b.jpg";
@@ -120,16 +124,23 @@ public class MainActivity extends Activity {
         dish = new Dish();
         dish.strURL = "https://www.allrecipes.com/thmb/9KDTW_QiOSvcPtSmYDoMvLZuYrg=/0x512/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/161586-papa-drexlers-bavarian-pretzels-DDMFS-001-4x3-4042ba41a6fa401e82aea394fa612f23.jpg";
         dish.name = "Bavarian Pretzels";
-        dish.ingredients_list = "• 8 ounces andouille sausage, cut into 1/4-inch cubes\n\n" +
-                "• 3 1/2 cups all-purpose flour, divided, or more as needed\n\n" +
-                "• 1 tablespoon active dry yeast\n\n" +
-                "• 1 teaspoon white sugar\n\n" +
-                "• 1 ⅓ cups water\n\n" +
-                "• 2 tablespoons butter, softened\n\n" +
-                "• ¼ teaspoon salt\n\n" +
-                "• 3 tablespoons baking soda\n\n" +
-                "• 2 tablespoons butter, melted\n\n" +
-                "• 1 tablespoon coarse sea salt, or to taste\n\n";
+        dish.ingredients_list = "• 3 1/2 cups all-purpose flour, divided, or more as needed\n" +
+                "\n" +
+                "• 1 tablespoon active dry yeast\n" +
+                "\n" +
+                "• 1 teaspoon white sugar\n" +
+                "\n" +
+                "• 1 ⅓ cups water\n" +
+                "\n" +
+                "• 2 tablespoons butter, softened\n" +
+                "\n" +
+                "• ¼ teaspoon salt\n" +
+                "\n" +
+                "• 3 tablespoons baking soda\n" +
+                "\n" +
+                "• 2 tablespoons butter, melted\n" +
+                "\n" +
+                "• 1 tablespoon coarse sea salt, or to taste";
         dish.steps_list = "1) Whisk 1 cup flour, yeast, and sugar together in a bowl. Stir in water and softened butter until well combined. Let stand until bubbles begin to form, about 15 minutes.\n\n" +
                 "2) Stir salt into yeast mixture, then gradually stir in 2 1/2 cups flour until dough can be picked up in a ball.\n\n" +
                 "3) Knead until smooth and elastic, about 8 minutes, adding more flour if needed.\n\n" +
